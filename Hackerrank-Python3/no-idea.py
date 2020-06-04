2b8ee25498cb2fd9585d4a91c6bff0504d409484
praveenkumarsrivas <pks101295@gmail.com>
2020-06-04 15:25:13 +0530

    no idea

--- Hackerrank-Python3/no-idea.py
+++ Hackerrank-Python3/no-idea.py
@@ -2,20 +2,15 @@ n, m = input().split()
 n = int(n)
 m = int(m)
 narr = list(map(int, input().split()))
-marr1 = list(map(int, input().split()))
-marr2 = list(map(int, input().split()))
-inter_sec_happy = []
-inter_sec_happy = list(set(narr) & set(marr1))
-# print(inter_sec_happy)
-h = 0
-h = len(inter_sec_happy)
+marr1 = set(map(int, input().split()))
+marr2 = set(map(int, input().split()))
+h=0
+for i in narr:
+    if i in marr1:
+        h+=1
+    if i in marr2:
+        h-=1
+print(h)
 
-inter_sec_sad = []
-inter_sec_sad = list(set(narr) & set(marr2))
-# print(inter_sec_sad)
-s = 0
-s = len(inter_sec_sad)
+#note: here marr1 and marr2 is not a list it's a set because while it was list the time taken by the compiler is much as compared to set in
 
-happy = abs(h-s)
-
-print(happy)
