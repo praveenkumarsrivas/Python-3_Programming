b9b1b8f34d3df2f541b696978bb751d643866b60
praveenkumarsrivas <pks101295@gmail.com>
2020-08-25 22:47:58 +0530

    just a commit

---
+++ Python-OOps/class-and-objects.py
@@ -0,0 +1,17 @@
+class Praveen:
+    names = []
+
+    def add_names(self, name):
+        self.names.append(name)
+
+    def show(self):
+        for x in names:
+            print(self.x)
+
+
+p = Praveen()
+p.add_names('pks')
+p.add_names('kumar')
+p.add_names('srivas')
+p.add_names('palera')
+p.show()
---
+++ Python-OOps/fun.py
@@ -0,0 +1,10 @@
+from time import sleep
+print("Hi! Praveen")
+sleep(1)
+print("your script for hacking the system '127.85.3.1' is initiated....")
+sleep(2)
+print("Connecting to server '127.85.3.1' ")
+sleep(2)
+print("system hacked!!!!")
+
+
